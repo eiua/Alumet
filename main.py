d5afc5e3f5653bd13b28beb1f821c2d9217baaac
Lucas Grigis <lucas.grigis@gmail.com>
2022-01-26 21:24:01 +0100

    ajout du main.py (renommage de lumet_v0.6_main.py vers main.py)

---
+++ main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""@author: lucas
+Fichier principal de Lumet
+Licence GPL euia / Lucas GRIGIS copyleft
+"""
+
+from gui import * # Import des classes de GUI (interface graphique) de lumet
+app = Application() # Création de l'objet Application qui est l'interface graphique (en Tkinter)
+app.mainloop() #Début d'une boucle infinie pour l'interface graphique
