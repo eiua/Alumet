6db73caf0c1790816417072c80ec1b95dcee5d22
Lucas Grigis <lucas.grigis@gmail.com>
2022-02-03 16:49:00 +0100

    Ajout de la carte de la neige (sprate) pour Arp 0.1°.

--- gui.py
+++ gui.py
@@ -231,6 +231,9 @@ class Application(Tk):
         menu_arpege_01.add_command(label="Précipitations", underline=3,
                                command=partial(self.DessinerCarteCumuls,
                                                "ARPEGE","0.1","Precips"))
+        menu_arpege_01.add_command(label="Hauteur de neige", underline=3,
+                               command=partial(self.DessinerCarteMonoParam,
+                                               "ARPEGE","0.1","Neige"))
         menu_arpege.add_cascade(label="ARPEGE 0.1°",
                              underline=0,menu=menu_arpege_01)
         menu_bar.add_cascade(label="ARPEGE", underline=0, menu=menu_arpege)
