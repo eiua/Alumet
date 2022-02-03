ec2b5151adecb6daf384f980cba53014645317ac
Lucas Grigis <lucas.grigis@gmail.com>
2022-02-03 15:29:34 +0100

    Ajout carte précipitations (tp) pour Arp 0.1°.

--- gui.py
+++ gui.py
@@ -228,6 +228,9 @@ class Application(Tk):
         menu_arpege_01.add_command(label="Pression au niveau de la mer", underline=3,
                                command=partial(self.DessinerCarteMonoParam,
                                                "ARPEGE","0.1","Pmer"))
+        menu_arpege_01.add_command(label="Précipitations", underline=3,
+                               command=partial(self.DessinerCarteCumuls,
+                                               "ARPEGE","0.1","Precips"))
         menu_arpege.add_cascade(label="ARPEGE 0.1°",
                              underline=0,menu=menu_arpege_01)
         menu_bar.add_cascade(label="ARPEGE", underline=0, menu=menu_arpege)
