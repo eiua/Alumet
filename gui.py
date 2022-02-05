b7c3095dfca3c2b4bb6b51ff21c3e5d92b046375
Lucas Grigis <lucas.grigis@gmail.com>
2022-02-05 14:59:50 +0100

    Ajout des cartes de Théta’W (Pseudo-Adiabatic Potential Temperature tatp) en niveaux isobares pour Aro 0.025°.

--- gui.py
+++ gui.py
@@ -229,6 +229,9 @@ class Application(Tk):
         menu_arome_0025_iso.add_command(label="TKE", underline=3,
                                command=partial(self.DessinerCarteMonoParam,
                                                "AROME","0.025","Turbulent_Kinetic_Energy_Iso"))
+        menu_arome_0025_iso.add_command(label="Theta’W", underline=3,
+                               command=partial(self.DessinerCarteMonoParam,
+                                               "AROME","0.025","Pseudo_Adiabatic_Potential_Temperature_Iso"))
         menu_arome_0025_tout.add_cascade(label="Niveaux isobares",
                              underline=0,menu=menu_arome_0025_iso)
         menu_arome.add_cascade(label="AROME 0.025°",
