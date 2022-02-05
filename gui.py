7da2635feb061fea01c98f1033c78183ab3568fb
Lucas Grigis <lucas.grigis@gmail.com>
2022-02-05 12:27:41 +0100

    Ajout de la carte du contenu spécifique des gouttes d’eau précipitantes (crwc) en niveaux isobares pour Aro 0.025°. Attention, comme pour toutes les cartes de valeurs spécifiques, il faudra adapter l’échelle des contours et de la colorbar.

--- gui.py
+++ gui.py
@@ -193,6 +193,9 @@ class Application(Tk):
         menu_arome_0025_iso.add_command(label="Contenu spécifique en eau liquide des nuages", underline=3,
                                command=partial(self.DessinerCarteMonoParam,
                                                "AROME","0.025","Specific_Cloud_Liquid_Water_Content_Iso"))
+        menu_arome_0025_iso.add_command(label="Contenu spécifique des gouttes d’eau précipitantes", underline=3,
+                               command=partial(self.DessinerCarteMonoParam,
+                                               "AROME","0.025","Specific_Rain_Water_Content_Iso"))
         menu_arome_0025_tout.add_cascade(label="Niveaux isobares",
                              underline=0,menu=menu_arome_0025_iso)
         menu_arome.add_cascade(label="AROME 0.025°",
